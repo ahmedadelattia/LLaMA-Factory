0cc955ead0df8be0be275f82ab1f76513cd635c9
ahmed <aadel@umd.edu>
2025-04-25 11:18:22 -0400

    remove audio_tower from language_model_keys in qwen2_5_omni_thinker registration

--- src/llamafactory/model/model_utils/visual.py
+++ src/llamafactory/model/model_utils/visual.py
@@ -272,7 +272,7 @@ _register_composite_model(
     model_type="qwen2_5_omni_thinker",
     projector_key="visual.merger",
     vision_model_keys=["visual.patch_embed", "visual.blocks"],
-    language_model_keys=["model", "lm_head", "audio_tower"],
+    language_model_keys=["model", "lm_head"],
     lora_conflict_keys=["patch_embed"],
 )
 
